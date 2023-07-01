8e22735f75641342a517351f65caa2b9aedcaa71
joeunSong <thdwhdms99@naver.com>
2023-07-01 22:03:41 +0900

    dev: firestore todos create

---
+++ dist/index.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var _reactNative = require("react-native");
+
+var _App = _interopRequireDefault(require("./App"));
+
+var _app = require("./app.json");
+
+require("react-native-get-random-values");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+/**
+ * @format
+ */
+_reactNative.AppRegistry.registerComponent(_app.name, function () {
+  return _App["default"];
+});
